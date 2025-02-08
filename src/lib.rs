468701a68407d13f8b1ae0151f83a85e43aa300a
Emmanuel Guefif <eguefif@fastmail.com>
2025-02-08 07:48:29 -0500

    Update lib interpret function with mod parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
+use crate::parser::Parser;
 use crate::tokenizer::lexer::Lexer;
 use std::fs;
 
+pub mod parser;
 pub mod tokenizer;
 
 pub fn interpret(filename: String) {
     let content = fs::read_to_string(filename).expect("Error: file does not exist");
     let lexer = Lexer::new(&content);
-    for token in lexer {
-        println!("{token:?}");
-    }
+    let parser = Parser::new(lexer);
 }
