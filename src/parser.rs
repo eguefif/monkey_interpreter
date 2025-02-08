8a87f5872fc41d3ab3fa8a528b5882c12be28030
Emmanuel Guefif <eguefif@fastmail.com>
2025-02-08 07:48:35 -0500

    Add struct parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,2 +1,25 @@
 pub mod ast_structs;
 pub mod ast_traits;
+
+use crate::tokenizer::lexer::Lexer;
+use crate::tokenizer::Token;
+
+pub struct Parser<'a> {
+    lexer: 'a Lexer,
+    curToken: Option<Token>,
+    nextToken: Option<Token>,
+}
+
+impl Parser<'_> {
+    pub fn new(lexer: Lexer) -> Self {
+        Self {
+            lexer,
+            curToken: None,
+            nextToken: None,
+        }
+    }
+
+    pub fn make_ast() -> String {
+        todo!()
+    }
+}
