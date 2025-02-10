48596ff8e9aed71c10a73626228fc1d78ccb69a2
Emmanuel Guefif <eguefif@fastmail.com>
2025-02-10 07:55:31 -0500

    implement ast using enums: basic
    We just handle Let Statement for now

--- src/parser.rs
+++ src/parser.rs
@@ -1,23 +1,18 @@
-use crate::parser::ast_structs::{Identifier, LetStatement, Program};
-use crate::parser::ast_traits::Statement;
+use crate::parser::ast_types::{Expression, Identifier, LetStatement, Program, Statement};
 use crate::tokenizer::lexer::Lexer;
 
-pub mod ast_structs;
-pub mod ast_traits;
+pub mod ast_types;
 
 pub struct Parser<'a> {
     lexer: Lexer<'a>,
 }
 
-impl<'a, T: Statement> Parser<'a> {
+impl<'a> Parser<'a> {
     pub fn new(lexer: Lexer<'a>) -> Self {
         Self { lexer }
     }
 
-    pub fn parse_program(&mut self) -> Program<T>
-    where
-        T: Statement,
-    {
+    pub fn parse_program(&mut self) -> Option<Program> {
         let token = self.lexer.next().unwrap();
         let s1 = LetStatement {
             token: token.clone(),
@@ -25,30 +20,46 @@ impl<'a, T: Statement> Parser<'a> {
                 token: token.clone(),
                 value: token.litteral.clone(),
             },
-            value: Identifier {
-                token: token.clone(),
+            value: Expression {
                 value: token.litteral.clone(),
             },
         };
-        let mut v: Vec<T> = Vec::new();
-        v.push(s1);
-        Program { statements: v }
+        let mut v: Vec<Statement> = Vec::new();
+        let stm = Statement::Let(s1);
+        v.push(stm);
+        Some(Program { statements: v })
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::parser::ast_types::Statement::Let;
     use crate::tokenizer::lexer::Lexer;
 
     #[test]
     fn it_should_parse_let_statement() {
-        let input = "let x =5;
+        let input = "let x = 5;
 let y = 10;
 let foobar = 838383;
     ";
         let lexer = Lexer::new(&input);
-        let parser = Parser::new(lexer);
-        let program = parser.parse_program();
+        let mut parser = Parser::new(lexer);
+        let program: Program = parser.parse_program().unwrap();
+        assert_eq!(program.len(), 3);
+
+        let expected_identifiers = ["x", "y", "foobar"];
+        for (i, stmt) in program.statements.iter().enumerate() {
+            assert!(check_let_statement(stmt, expected_identifiers[i]));
+        }
+    }
+
+    fn check_let_statement(smt: &Statement, identifiers: &str) -> bool {
+        if let Let(smt) = smt {
+            if smt.identifier.value.as_str() == identifiers {
+                return true;
+            }
+        }
+        false
     }
 }
---
+++ src/parser/ast_types.rs
@@ -0,0 +1,44 @@
+use crate::tokenizer::Token;
+
+pub enum Node {
+    Program(Program),
+    Statement(Statement),
+    Expression(Expression),
+}
+
+pub enum Statement {
+    Let(LetStatement),
+    Return,
+    Expr,
+}
+
+pub struct Program {
+    pub statements: Vec<Statement>,
+}
+
+impl Program {
+    pub fn new() -> Self {
+        Self {
+            statements: Vec::with_capacity(200),
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.statements.len()
+    }
+}
+
+pub struct LetStatement {
+    pub token: Token,
+    pub identifier: Identifier,
+    pub value: Expression,
+}
+
+pub struct Expression {
+    pub value: String,
+}
+
+pub struct Identifier {
+    pub token: Token,
+    pub value: String,
+}
