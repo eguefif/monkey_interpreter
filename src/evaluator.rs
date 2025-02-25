ff2e1a8e9b0335532983d2973fe6707703c476ad
Emmanuel Guefif <eguefif@fastmail.com>
2025-02-24 20:43:17 -0500

    Evaluate infix expression

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,6 +1,8 @@
 use crate::{
     object::{BoolObject, Int, Null, Object, ObjectType},
-    parser::ast_types::{Bool, Expression, Integer, PrefixExpression, PrefixType, Statement},
+    parser::ast_types::{
+        Bool, Expression, InfixType, Integer, PrefixExpression, PrefixType, Statement,
+    },
 };
 
 pub fn evaluate(stmt: &Statement) -> Option<Object> {
@@ -13,14 +15,76 @@ pub fn evaluate(stmt: &Statement) -> Option<Object> {
 fn evaluate_expression(exp: &Expression) -> Option<Object> {
     match exp {
         Expression::PrefixOp(prefix) => {
-            let right =
-                evaluate_expression(&prefix.right).expect("Error while evaluating expression");
+            let right = evaluate_expression(&prefix.right)
+                .expect("Error while evaluating prefix expression");
             evaluate_prefix(prefix, right)
         }
         Expression::Int(int) => Some(make_int(int)),
         Expression::Boolean(boolean) => Some(make_bool(boolean)),
+        Expression::InfixOp(infix) => {
+            let right =
+                evaluate_expression(&infix.right).expect("error while evaluting infix expression");
+
+            let left =
+                evaluate_expression(&infix.left).expect("error while evaluting infix expression");
+            evaluate_infix(&infix.infix_type, left, right)
+        }
         _ => None,
     }
+}
+
+fn evaluate_infix(op: &InfixType, left: Object, right: Object) -> Option<Object> {
+    if let ObjectType::Int(right) = right.obj_type {
+        if let ObjectType::Int(left) = left.obj_type {
+            match op {
+                InfixType::Add => {
+                    return Some(Object::new(ObjectType::Int(Int {
+                        value: right.value + left.value,
+                    })))
+                }
+                InfixType::Sub => {
+                    return Some(Object::new(ObjectType::Int(Int {
+                        value: left.value - right.value,
+                    })))
+                }
+                InfixType::Mul => {
+                    return Some(Object::new(ObjectType::Int(Int {
+                        value: right.value * left.value,
+                    })))
+                }
+                InfixType::Div => {
+                    if right.value == 0 {
+                        panic!("Div by 0 is forbidden")
+                    }
+                    return Some(Object::new(ObjectType::Int(Int {
+                        value: left.value / right.value,
+                    })));
+                }
+                InfixType::Gt => {
+                    return Some(Object::new(ObjectType::Bool(BoolObject {
+                        value: left.value > right.value,
+                    })));
+                }
+                InfixType::Lt => {
+                    return Some(Object::new(ObjectType::Bool(BoolObject {
+                        value: left.value < right.value,
+                    })));
+                }
+                InfixType::Eq => {
+                    return Some(Object::new(ObjectType::Bool(BoolObject {
+                        value: left.value == right.value,
+                    })));
+                }
+                InfixType::Noteq => {
+                    return Some(Object::new(ObjectType::Bool(BoolObject {
+                        value: left.value != right.value,
+                    })));
+                }
+                InfixType::None => return Some(Object::new(ObjectType::Null(Null {}))),
+            }
+        }
+    }
+    Some(Object::new(ObjectType::Null(Null {})))
 }
 
 fn evaluate_prefix(prefix: &PrefixExpression, right: Object) -> Option<Object> {
@@ -138,6 +202,32 @@ mod tests {
         for (input, expected) in tests {
             let obj = test_eval(input);
             assert_bool(obj, expected)
+        }
+    }
+
+    #[test]
+    fn it_should_eval_infix() {
+        let tests = [
+            ("5", 5),
+            ("10", 10),
+            ("-5", -5),
+            ("-10", -10),
+            ("5 + 5 + 5 + 5 - 10", 10),
+            ("2 * 2 * 2 * 2 * 2", 32),
+            ("-50 + 100 + -50", 0),
+            ("5 * 2 + 10", 20),
+            ("5 + 2 * 10", 25),
+            ("20 + 2 * -10", 0),
+            ("50 / 2 * 2 + 10", 60),
+            ("2 * (5 + 10)", 30),
+            ("3 * 3 * 3 + 10", 37),
+            ("3 * (3 * 3) + 10", 37),
+            ("(5 + 10 * 2 + 15 / 3) * 2 + -10", 50),
+        ];
+
+        for (input, expected) in tests {
+            let obj = test_eval(input);
+            assert_int(obj, expected)
         }
     }
 }
